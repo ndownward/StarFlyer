1b893c7e97f4080e6cdd15e7092938f173f65e7c
Nicola Downward <nicoladownward@dal.ca>
2022-12-10 13:05:34 -0400

    create functional key methods

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,11 +13,24 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ProcessThrust();
+        ProcessRotation();
     }
 
-    void ProcessInput() {
-        Input.GetKey(KeyCode.);
+    void ProcessThrust() {
+        if (Input.GetKey(KeyCode.Space)){
+            Debug.Log("Pressed SPACE - thrusting");
+        }
+    }
+
+    void ProcessRotation() {
+        if (Input.GetKey(KeyCode.A)){
+            Debug.Log("Rotating left");
+        }
+
+        else if (Input.GetKey(KeyCode.D)){
+            Debug.Log("Rotating right");
+        }
 
     }
 }
