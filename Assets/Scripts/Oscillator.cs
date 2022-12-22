7ef9cce7ef1ea986b41869f87d7df2053de7d18d
Nicola Downward <nicoladownward@dal.ca>
2022-12-22 13:10:09 -0400

    added functionality for moving obstacles

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
+
+    Vector3 startingPos;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
+        Debug.Log(startingPos);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
     }
 }
